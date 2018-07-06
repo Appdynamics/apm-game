7377db1b319f4ce075c34979078050cced34034e
Severin Neumann <severin.neumann@altmuehlnet.de>
2018-07-06 08:44:41 +0200

    Process services in master

--- master/index.js
+++ master/index.js
@@ -1,9 +1,21 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
+const process = require('process')
+
 try {
-    const config = yaml.safeLoad(fs.readFileSync('test.yml', 'utf8'));
-    const indentedJson = JSON.stringify(config, null, 4);
-    console.log(indentedJson);
+    const config = yaml.safeLoad(fs.readFileSync(process.argv[2], 'utf8'));
+    const {global, services, transactions} = config
+
+    if(typeof services !== 'object') {
+      console.log('Could not read services list!')
+      process.exit()
+    }
+
+    Object.keys(services).forEach(function (service) {
+      const properties = services[service]
+      console.log(`Run agent of type ${properties.type}, agent: ${properties.agent}`)
+    })
+
 } catch (e) {
     console.log(e);
 }
