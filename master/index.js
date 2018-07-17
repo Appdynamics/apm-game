85f5abe550cc12e6300fa55ef73bcb8e0c54b789
Severin Neumann <severin.neumann@altmuehlnet.de>
2018-07-17 13:57:02 +0200

    Update master

--- master/index.js
+++ master/index.js
@@ -1,6 +1,10 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const process = require('process')
+const { spawn } = require('child_process')
+const url = require('url');
+
+console.log()
 
 try {
     const config = yaml.safeLoad(fs.readFileSync(process.argv[2], 'utf8'));
@@ -11,10 +15,46 @@ try {
       process.exit()
     }
 
+    var port = 3000
     Object.keys(services).forEach(function (service) {
+
+      services[service].port = port
+      services[service].host = 'localhost'
+
       const properties = services[service]
-      console.log(`Run agent of type ${properties.type}, agent: ${properties.agent}`)
+      const child = spawn(`node ${__dirname}/../nodes/nodejs/index.js ${properties.port}`, { shell: true })
+
+      child.stdout.on('data', (data) => {
+        console.log(`${service} stdout:\n${data}`);
+      });
+
+      port++
     })
+
+    function buildTransaction(transactionName, transactions) {
+
+      console.log(transactionName)
+
+      const transactionUrl = url.parse(transactionName)
+
+      var body = null
+
+      if(Array.isArray(transactions[transactionName])) {
+        body = transactions[transactionName].map(t => buildTransaction(t, transactions[transactionName]))
+      }
+
+      const transaction = {
+        protocol: transactionUrl.protocol,
+        host: transactionUrl.host,
+        port: transactionUrl.port,
+        path: transactionUrl.path,
+        body: body
+      }
+
+      return transaction
+    }
+
+    console.log(Object.keys(transactions).map(t => buildTransaction(t, transactions)))
 
 } catch (e) {
     console.log(e);
