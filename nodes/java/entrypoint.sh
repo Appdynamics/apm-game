67a9c7d5f3815e157951d9fe0c71d7d96742faa7
Severin Neumann <severin.neumann@altmuehlnet.de>
2019-09-19 16:00:14 +0200

    Update docker images

--- nodes/java/entrypoint.sh
+++ nodes/java/entrypoint.sh
@@ -12,12 +12,12 @@ then
 	echo "Running with agent..."
 	if [ "$WITH_ANALYTICS" -eq "1" ]
 	then
-		java -javaagent:/opt/appdynamics/javaagent.jar ${LIVEDEBUG} -Djava.security.egd=file:/dev/urandom -Dappdynamics.socket.collection.bci.enable=true -Dappdynamics.analytics.agent.url=http://machine-agent:9090/v2/sinks/bt  -jar "javanode-1.0-SNAPSHOT.jar" 80
+		java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -javaagent:/opt/appdynamics/javaagent.jar ${LIVEDEBUG} -Djava.security.egd=file:/dev/urandom -Dappdynamics.socket.collection.bci.enable=true -Dappdynamics.analytics.agent.url=http://machine-agent:9090/v2/sinks/bt  -jar "javanode-1.0-SNAPSHOT.jar" 80
 	else
 		# Also without analytics we want to have docker monitoring, so the additional -Dappdynamics.dockerMonitoring=true is a hint for the machine agent.
-		java -javaagent:/opt/appdynamics/javaagent.jar ${LIVEDEBUG} -Djava.security.egd=file:/dev/urandom -Dappdynamics.socket.collection.bci.enable=true -Dappdynamics.dockerMonitoring=true -jar "javanode-1.0-SNAPSHOT.jar" 80
+		java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -javaagent:/opt/appdynamics/javaagent.jar ${LIVEDEBUG} -Djava.security.egd=file:/dev/urandom -Dappdynamics.socket.collection.bci.enable=true -Dappdynamics.dockerMonitoring=true -jar "javanode-1.0-SNAPSHOT.jar" 80
 	fi
 else
 	echo "Running without agent..."
-	java -jar "javanode-1.0-SNAPSHOT.jar" 80
+	java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar "javanode-1.0-SNAPSHOT.jar" 80
 fi
